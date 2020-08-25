7fe335afd2b871beaf334f205ab059a844759cd2
naina321 <naina1910078@akgec.ac.in>
2020-08-25 15:22:29 +0530

    using svg

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
+function Move()
+{
+  var elem = document.getElementById("shin");  
+  var kit = document.getElementById("kite"); 
+  var pos = 0;
+  var id = setInterval(frame, 7);
+  function frame() {
+    if (pos ==950) {
+     pos = 0;
+      /*clearInterval(id);*/
+    } else {
+      pos++; 
+      elem.style.right = pos + 'px'; 
+      kit.style.top=pos+"px";
+      kit.style.left=pos+"px";
 
+    }
+  }
+}
